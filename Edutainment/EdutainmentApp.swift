acb1d46b8fc4e7463aee6d64e8543411819e070d
sebastian.popa <sebastian.popa@softvision.com>
2023-08-06 17:04:28 +0300

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by sebastian.popa on 8/6/23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
